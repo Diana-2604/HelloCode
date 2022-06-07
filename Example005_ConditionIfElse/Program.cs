256a8e749bf222664ae39b3092ac1cf312121dfe
Diana-2604 <Diana8991@yandex.ru>
2022-06-07 15:58:13 +0300

    add Example005_ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Please enter your name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "diana")
+{
+    Console.WriteLine("Hurray, DIANA is here!");
+}
+else
+{
+    Console.WriteLine("Hello, ");
+    Console.Write(username);
+}
