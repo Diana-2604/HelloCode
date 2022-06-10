2fdf22836c89990e9d84b894d7238e9ca3371ee4
Diana-2604 <Diana8991@yandex.ru>
2022-06-10 19:41:10 +0300

    add Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿// Find element in an array
+
+int[] array = {32,43,54,6500,43,65,765,75,54};
+
+int n = array.Length;
+int find = 54; // find element 5 in the array
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // interrupt algorithm when the first example is found
+    }
+    index++;
+}
